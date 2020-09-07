7987b7579723295f63f9d2b9aabaf26c104d52fb
martinidry07 <70594234+martinidry07@users.noreply.github.com>
2020-09-07 18:57:30 +0300

    new day, new work

---
+++ Day06/Day06/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Day06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sample();
+        }
+        private static void Sample()
+        {
+            Console.WriteLine("Hello World!");
+        }
+        private static void ListSample()
+        {
+            List<String> lst = new List<String>();
+            String choise = "";
+
+            while (choise!="0")
+            {
+                Console.WriteLine("1 - Izvadīt");
+                Console.WriteLine("2 - Pievienot");
+                Console.WriteLine("0 - Iziet");
+                choise = Console.ReadLine();
+                
+                switch(choise)
+                {
+                    case "0":
+                        break;
+                    case "1":
+                        break;
+                    case "2":
+                        
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+        }
+    }
+}
