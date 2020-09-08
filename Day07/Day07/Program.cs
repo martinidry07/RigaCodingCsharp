a59fcea584b1fc10c08ba97704f93478d16053ba
martinidry07 <70594234+martinidry07@users.noreply.github.com>
2020-09-08 19:33:20 +0300

    Work with objects

---
+++ Day07/Day07/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Day07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //sample();
+            Task01();
+
+        }
+        private static void sample()
+        {
+            Console.WriteLine("Hello World!");
+        }
+        private static void Task01()
+        {
+            Console.WriteLine("Ievadam studentu Nr.1:");
+            
+            Console.Write("Vārds: ");
+            String name = Console.ReadLine();
+            Console.Write("Uzvārds: ");
+            String surname = Console.ReadLine();
+            int course = InputNumber("Kurss: ");
+            Student student1 = new Student(name, surname, course);
+
+            Console.WriteLine("Ievadam studentu Nr.2:");
+            Console.Write("Vārds: ");
+             name = Console.ReadLine();
+            Console.Write("Uzvārds: ");
+             surname = Console.ReadLine();
+             course = InputNumber("Kurss: ");
+            Student student2 = new Student(name, surname, course);
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Izvade:");
+            student1.PrintInfo();
+            student2.PrintInfo();
+        }
+        private static int InputNumber(String text)
+        {
+
+            int number = 0;
+
+
+            Console.Write(text);
+            String input = Console.ReadLine();
+            try
+            {
+                number = Convert.ToInt32(input);
+            }
+            catch
+            {
+
+                return InputNumber("Ievade neatbilst nosacījumiem ! Mēģini vēlreiz: ");
+            }
+            ;
+            return number;
+        }
+    }
+}
---
+++ Day07/Day07/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day07
+{
+    class Student
+    {
+        public String Name;
+        public String Surname;
+        public int Course;
+
+        public Student (String _name, String _surname, int _course)
+        {
+            this.Name = _name;
+            this.Surname = _surname;
+            if (_course > 3)
+            {
+                this.Course  = 3;
+            }
+            else if (_course < 1)
+            {
+                this.Course  = 1;
+            }
+            else
+            {
+                this.Course  = _course;
+            }
+        }
+        
+        public void PrintInfo()
+        {
+            Console.WriteLine(Name + " " + Surname + " " + Course + ". kurss");
+        }
+    }
+}
